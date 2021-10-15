e7647761a44105b90d1b8133bcec5c861a1d571d
fesavali <felixnzioki99@gmail.com>
2021-10-15 09:27:52 +0300

    retrieve data

--- app/Http/Controllers/fleetRecords.php
+++ app/Http/Controllers/fleetRecords.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Vehicle;
 
 class fleetRecords extends Controller
 {
    public function index(){
-       return view('records');
+       $vehicles = Vehicle::paginate(11);
+       return view('records', compact('vehicles'));
    }
 }
--- resources/views/records.blade.php
+++ resources/views/records.blade.php
@@ -67,71 +67,30 @@
               <table class="table mb-0">
                 <thead>
                   <tr>
-                    <th scope="col">EMPLOYEES</th>
-                    <th scope="col">POSITION</th>
-                    <th scope="col">CONTACTS</th>
-                    <th scope="col">AGE</th>
-                    <th scope="col">ADDRESS</th>
-                    <th scope="col">SALARY</th>
+                  <th scope="col">#</th>
+                    <th scope="col">Vehicle Registration</th>
+                    <th scope="col">Client</th>
+                    <th scope="col">Make</th>
+                    <th scope="col">Model</th>
+                    <th scope="col">Chassis Number</th>
+                    <th scope="col">Engine</th>
+                    <th scope="col">Color</th>
+                    
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <th scope="row" style="color: #666666;">Tiger Nixon</th>
-                    <td>System Architect</td>
-                    <td>tnixon12@example.com</td>
-                    <td>61</td>
-                    <td>Edinburgh</td>
-                    <td>$320,800</td>
+                  @foreach($vehicles as $vehicle)
+                    <th scope="row" style="color: #666666;">{{ $vehicle->id }}</th>
+                    <td>{{ $vehicle->vehicle_registration_number }}</td>
+                    <td>{{ $vehicle->client_details }}</td>
+                    <td>{{ $vehicle->vehicle_make }}</td>
+                    <td>{{ $vehicle->vehicle_model }}</td>
+                    <td>{{ $vehicle->chassis_number }}</td>
+                    <td>{{ $vehicle->engine_number }}</td>
+                    <td>{{ $vehicle->color }}</td>
                   </tr>
-                  <tr>
-                    <th scope="row" style="color: #666666;">Sonya Frost</th>
-                    <td>Software Engineer</td>
-                    <td>sfrost34@example.com</td>
-                    <td>23</td>
-                    <td>Edinburgh</td>
-                    <td>$103,600</td>
-                  </tr>
-                  <tr>
-                    <th scope="row" style="color: #666666;">Jena Gaines</th>
-                    <td>Office Manager</td>
-                    <td>jgaines75@example.com</td>
-                    <td>30</td>
-                    <td>London</td>
-                    <td>$90,560</td>
-                  </tr>
-                  <tr>
-                    <th scope="row" style="color: #666666;">Quinn Flynn</th>
-                    <td>Support Lead</td>
-                    <td>qflyn09@example.com</td>
-                    <td>22</td>
-                    <td>Edinburgh</td>
-                    <td>$342,000</td>
-                  </tr>
-                  <tr>
-                    <th scope="row" style="color: #666666;">Charde Marshall</th>
-                    <td>Regional Director</td>
-                    <td>cmarshall28@example.com</td>
-                    <td>36</td>
-                    <td>San Francisco</td>
-                    <td>$470,600</td>
-                  </tr>
-                  <tr>
-                    <th scope="row" style="color: #666666;">Haley Kennedy</th>
-                    <td>Senior Marketing Designer</td>
-                    <td>hkennedy63@example.com</td>
-                    <td>43</td>
-                    <td>London</td>
-                    <td>$313,500</td>
-                  </tr>
-                  <tr>
-                    <th scope="row" style="color: #666666;">Tatyana Fitzpatrick</th>
-                    <td>Regional Director</td>
-                    <td>tfitzpatrick00@example.com</td>
-                    <td>19</td>
-                    <td>Warsaw</td>
-                    <td>$385,750</td>
-                  </tr>
+                @endforeach
                 </tbody>
               </table>
             </div>
