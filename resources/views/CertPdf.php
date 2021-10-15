e35adb008e67c07a0b5be52ef2c2e14d0308afb5
fesavali <felixnzioki99@gmail.com>
2021-10-15 11:45:41 +0300

    update pdf background

--- resources/views/CertPdf.php
+++ resources/views/CertPdf.php
@@ -3,7 +3,7 @@ include('../App/fpdf182/fpdf.php');
 $pdf = new FPDF('P', 'mm', 'A4' );
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
+$fpdf->Image('../resources/fleet.jpg', 0, 0, $fpdf->w, $fpdf->h);
 if (preg_match("/MSIE/i", $_SERVER["HTTP_USER_AGENT"])){
     header("Content-type: application/PDF");
 } else {
