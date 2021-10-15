562d7322775842812561c4a56d3fdf829342c50f
fesavali <felixnzioki99@gmail.com>
2021-10-15 08:44:08 +0300

    create records page

---
+++ resources/views/records.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Fleet Records</title>
+    <!-- Favicon -->
+    <link href="{{ asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <!-- Extra details for Live View on GitHub Pages -->
+
+    <!-- Icons -->
+    <link href="{{ asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+    <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <!-- Argon CSS -->
+    <link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
+</head>
+<body class="{{ $class ?? '' }}">
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<section class="intro">
+  <div class="gradient-custom-1 h-100">
+    <div class="mask d-flex align-items-center h-100">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12">
+            <div class="table-responsive bg-white">
+              <table class="table mb-0">
+                <thead>
+                  <tr>
+                    <th scope="col">EMPLOYEES</th>
+                    <th scope="col">POSITION</th>
+                    <th scope="col">CONTACTS</th>
+                    <th scope="col">AGE</th>
+                    <th scope="col">ADDRESS</th>
+                    <th scope="col">SALARY</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row" style="color: #666666;">Tiger Nixon</th>
+                    <td>System Architect</td>
+                    <td>tnixon12@example.com</td>
+                    <td>61</td>
+                    <td>Edinburgh</td>
+                    <td>$320,800</td>
+                  </tr>
+                  <tr>
+                    <th scope="row" style="color: #666666;">Sonya Frost</th>
+                    <td>Software Engineer</td>
+                    <td>sfrost34@example.com</td>
+                    <td>23</td>
+                    <td>Edinburgh</td>
+                    <td>$103,600</td>
+                  </tr>
+                  <tr>
+                    <th scope="row" style="color: #666666;">Jena Gaines</th>
+                    <td>Office Manager</td>
+                    <td>jgaines75@example.com</td>
+                    <td>30</td>
+                    <td>London</td>
+                    <td>$90,560</td>
+                  </tr>
+                  <tr>
+                    <th scope="row" style="color: #666666;">Quinn Flynn</th>
+                    <td>Support Lead</td>
+                    <td>qflyn09@example.com</td>
+                    <td>22</td>
+                    <td>Edinburgh</td>
+                    <td>$342,000</td>
+                  </tr>
+                  <tr>
+                    <th scope="row" style="color: #666666;">Charde Marshall</th>
+                    <td>Regional Director</td>
+                    <td>cmarshall28@example.com</td>
+                    <td>36</td>
+                    <td>San Francisco</td>
+                    <td>$470,600</td>
+                  </tr>
+                  <tr>
+                    <th scope="row" style="color: #666666;">Haley Kennedy</th>
+                    <td>Senior Marketing Designer</td>
+                    <td>hkennedy63@example.com</td>
+                    <td>43</td>
+                    <td>London</td>
+                    <td>$313,500</td>
+                  </tr>
+                  <tr>
+                    <th scope="row" style="color: #666666;">Tatyana Fitzpatrick</th>
+                    <td>Regional Director</td>
+                    <td>tfitzpatrick00@example.com</td>
+                    <td>19</td>
+                    <td>Warsaw</td>
+                    <td>$385,750</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    <!-- Copyright -->
+    <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2020 Copyright:
+      <a class="text-dark" href="https://mdbootstrap.com/"
+         >MDBootstrap.com</a
+        >
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</div>
+<!-- End of .container -->
+</body>
+<style>
+    html,
+body,
+.intro {
+  height: 100%;
+}
+
+.gradient-custom-1 {
+  /* fallback for old browsers */
+  background: #cd9cf2;
+
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: -webkit-linear-gradient(to top, rgba(205, 156, 242, 1), rgba(246, 243, 255, 1));
+
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: linear-gradient(to top, rgba(205, 156, 242, 1), rgba(246, 243, 255, 1))
+}
+
+table td,
+table th {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+}
+tbody td {
+  font-weight: 500;
+  color: #999999;
+}
+</style>
+</html>
