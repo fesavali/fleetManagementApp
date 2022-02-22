8a28fa16832bfcffc8cbc7200b436f624a3d8e2f
fesavali <felixnzioki99@gmail.com>
2022-02-22 09:59:48 +0300

    update

--- resources/views/CertPdf.blade.php
+++ resources/views/CertPdf.blade.php
@@ -14,31 +14,31 @@ $x = $pdf->GetX();
 $y = $pdf->GetY();
 $pdf->SetXY($x + 120, $y +3);
 $pdf->SetFont('Times','BU',14);
-$pdf->Cell(55, 5,"Serial No: $vehicle->serial", 0, 0);
+// $pdf->Cell(55, 5,"Serial No: $vehicle->serial", 0, 0);
 
 
 $pdf->Ln(0);
 $x = $pdf->GetX();
 $y = $pdf->GetY();
-$pdf->SetXY($x + 49, $y +104);
+$pdf->SetXY($x + 49, $y +107);
 $pdf->SetFont('Times','',14);
 $pdf->Cell(55, 5, date("d/m/Y", strtotime($vehicle->instal_date)), 0, 0);
 $pdf->Ln(1);
 $x = $pdf->GetX();
 $y = $pdf->GetY();
-$pdf->SetXY($x + 85, $y +20);
+$pdf->SetXY($x + 85, $y +19);
 $pdf->SetFont('Times','',12);
 $pdf->Cell(55, 5, $vehicle->vehicle_registration_number, 0, 0);
 $pdf->Ln(2);
 $x = $pdf->GetX();
 $y = $pdf->GetY();
-$pdf->SetXY($x + 85, $y +8);
+$pdf->SetXY($x + 85, $y +7);
 $pdf->SetFont('Times','',12);
 $pdf->Cell(55, 5, $vehicle->client_details, 0, 0);
 $pdf->Ln(3);
 $x = $pdf->GetX();
 $y = $pdf->GetY();
-$pdf->SetXY($x + 85, $y +7);
+$pdf->SetXY($x + 85, $y +6);
 $pdf->SetFont('Times','',12);
 $pdf->Cell(55, 5, $vehicle->vehicle_make, 0, 0);
 $pdf->Ln(4);
@@ -74,7 +74,7 @@ $pdf->Cell(55, 5, $vehicle->other_interest, 0, 0);
 $pdf->Ln(9);
 $x = $pdf->GetX();
 $y = $pdf->GetY();
-$pdf->SetXY($x + 45, $y +14);
+$pdf->SetXY($x + 45, $y +12);
 $pdf->SetFont('Times','',12);
 $pdf->Cell(55, 5, date("d/m/Y", strtotime($vehicle->expiry_date)), 0, 0);
 $pdf->Ln(10);
@@ -94,7 +94,7 @@ $pdf->setTextColor(0,101,68);
 $pdf->setFillColor(255,255,255, 0.9); 
 // $pdf->Cell(55, 5, "Subscription Expiry Date", 0, 0, 'C', TRUE);
 
-$pdf->Image($img2,135,223,25,12);
+// $pdf->Image($img2,135,223,25,12);
 
 if (preg_match("/MSIE/i", $_SERVER["HTTP_USER_AGENT"])){
     header("Content-type: application/PDF");
